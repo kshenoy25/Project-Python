73da765ccb29733e2e6351409e91dadf0c20ca92
KSCode <kunalshenoy25@gmail.com>
2024-11-12 23:57:34 -0600

    Update

--- PythonTypingTest/typingTest.py
+++ PythonTypingTest/typingTest.py
@@ -8,17 +8,36 @@ def start_screen(stdscr):
     stdscr.refresh()
     stdscr.getkey()
 
+
+def display_text(stdscr, target, current, wpm = 0):
+    stdscr.addstr(target)
+
+    for i, char in enumerate(current):
+        correct_char = target[i]
+        color = curses.color_pair(1)
+        if char != correct_char:
+            color = curses.color_pair(2)
+        stdscr.addstr(0, i, char, color)
+
+
+
 def wpm_test(stdscr):
     target_text = "Hello world this is some test text for this app!"
     current_text = []
 
-
-
     while True:
+        stdscr.clear()
+        display_text(stdscr, target_text, current_text)
+        stdscr.refresh()
+
         key = stdscr.getkey()
         if ord(key) == 27: # numeric ascii representation 
             break
-        current_text.append(key)
+        if key in ("KEY_BACKSPACE", '\b', "\x7f"):
+            if len(current_text) > 0:
+                current_text.pop()
+        else:
+            current_text.append(key)
 
         stdscr.clear()
         stdscr.addstr(target_text)
