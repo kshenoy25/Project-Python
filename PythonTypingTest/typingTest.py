dfe70f45975f5f631fdeb8ed362c335482fd3548
KSCode <kunalshenoy25@gmail.com>
2024-11-14 11:43:07 -0600

    Project Python
    More projects!!!!!

--- PythonTypingTest/typingTest.py
+++ PythonTypingTest/typingTest.py
@@ -1,5 +1,7 @@
 import curses
 from curses import wrapper
+import time
+import random
 
 def start_screen(stdscr):
     stdscr.clear()
@@ -11,6 +13,7 @@ def start_screen(stdscr):
 
 def display_text(stdscr, target, current, wpm = 0):
     stdscr.addstr(target)
+    stdscr.addstr(1, 0, f"WPM: {wpm}")
 
     for i, char in enumerate(current):
         correct_char = target[i]
@@ -19,29 +22,45 @@ def display_text(stdscr, target, current, wpm = 0):
             color = curses.color_pair(2)
         stdscr.addstr(0, i, char, color)
 
+def load_text():
+    with open("text.txt", "r") as f:
+        lines = f.readlines()
+        return random.choice(lines).strip() # randomly chooses elements from the list and .strip gets rid of the user having to type in \n and strips any white text
 
 
 def wpm_test(stdscr):
-    target_text = "Hello world this is some test text for this app!"
+    target_text = load_text()
     current_text = []
-
+    wpm = 0
+    start_time = time.time()
+    stdscr.nodelay(True)
     while True:
+        time_elapsed = max(time.time() - start_time, 1)
+        wpm = round(len(current_text) / (time_elapsed / 60)) / 5
+
         stdscr.clear()
-        display_text(stdscr, target_text, current_text)
+        display_text(stdscr, target_text, current_text, wpm)
         stdscr.refresh()
 
-        key = stdscr.getkey()
+        # check if the users current text is equal to the target text
+        if "".join(current_text) == target_text: # seperator combines every character from the list
+            stdscr.nodelay(False)
+            break
+        try:
+            key = stdscr.getkey() # blocking, waiting for user to enter a key
+        except:
+            continue
+
         if ord(key) == 27: # numeric ascii representation 
             break
         if key in ("KEY_BACKSPACE", '\b', "\x7f"):
             if len(current_text) > 0:
                 current_text.pop()
-        else:
+        elif len(current_text) < len(target_text) :
             current_text.append(key)
 
         stdscr.clear()
         stdscr.addstr(target_text)
-
 
         for char in current_text:
             stdscr.addstr(char, curses.color_pair(1))
@@ -55,10 +74,12 @@ def main(stdscr):
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
 
     start_screen(stdscr)
-    wpm_test(stdscr)
+    while True:
+        wpm_test(stdscr)
+        stdscr.addstr(2,0, "Text completed! Press any key to continue...")
+        key = stdscr.getkey()
 
-
-    key = stdscr.getkey()
-    print(key)
+        if ord(key) == 27:
+            break
 
 wrapper(main)
